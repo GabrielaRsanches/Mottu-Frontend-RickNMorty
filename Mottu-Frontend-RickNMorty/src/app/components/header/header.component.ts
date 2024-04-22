02b3973ee33c794b6218276efdc90fd728e6dcb1
Gabriela Sanches <gabi@toki.life>
2024-04-22 20:42:04 -0300

    feat: header and home components ready

--- Mottu-Frontend-RickNMorty/src/app/components/header/header.component.ts
+++ Mottu-Frontend-RickNMorty/src/app/components/header/header.component.ts
@@ -1,67 +1,88 @@
 import { Component, EventEmitter, Inject, inject, NO_ERRORS_SCHEMA, Output } from '@angular/core';
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
-import {MatButton, MatButtonModule} from '@angular/material/button';
-import {MatToolbar, MatToolbarModule, MatToolbarRow} from '@angular/material/toolbar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatToolbar, MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
 import { FavoritesComponent } from '../favorites/favorites.component';
+import { FavoriteCharactersService } from '../favorites/favorites.service';
 
 @Component({
   selector: 'app-header',
   template: `
-  <head>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-  <script type="importmap">
-    {
-      "imports": {
-        "@material/web/": "https://esm.run/@material/web/"
-      }
-    }
-  </script>
-  <script type="module">
-    import '@material/web/all.js';
-    import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
+    <head class="header">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <script type="importmap">
+        {
+          "imports": {
+            "@material/web/": "https://esm.run/@material/web/"
+          }
+        }
+      </script>
+      <script type="module">
+        import '@material/web/all.js';
+        import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 
-    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
-  </script>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="../../../../node_modules/@angular/material/prebuilt-themes/indigo-pink.css" rel="stylesheet">
-  </head>
-  <mat-toolbar style="width:100%, weight:100%">
-      <span>My App</span>
-      <button style="padding: 1em;" mat-icon-button (click)="navigateToHome()" class="example-icon home-icon" aria-label="Example icon-button with home icon">
-        <mat-icon fontIcon="home"></mat-icon>
-      </button>
-      <span class="example-spacer"></span>
-
-            <button style="padding: 1em;" mat-icon-button (click)="navigateToFavorites()" class="example-icon favorite-icon" aria-label="Example icon-button with heart icon">
-              <mat-icon fontIcon="favorites"></mat-icon>
-            </button>
-
+        document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
+      </script>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    </head>
+    <mat-toolbar class="header-component">
+      <img src="../../assets/logo.png" />
+      <div class="menu-icons">
+        <button
+          title="Início"
+          mat-icon-button
+          (click)="navigateToHome()"
+          class="example-icon header-icons"
+          aria-label="home"
+        >
+          <mat-icon id="icon" fontIcon="home"></mat-icon>
+        </button>
+        <button mat-icon-button (click)="navigateToFavorites()" class="header-icons">
+          <mat-icon id="icon" fontIcon="favorites"></mat-icon>
+          <p>{{ favoritesCount }}</p>
+        </button>
+      </div>
     </mat-toolbar>
-<router-outlet></router-outlet>
-<script src="node_modules/angular/angular.min.js"></script>
-<script src="node_modules/angular/material/toolbar"></script>
 
+    <router-outlet></router-outlet>
+    <script src="node_modules/angular/angular.min.js"></script>
+    <script src="node_modules/angular/material/toolbar"></script>
   `,
   styleUrls: ['../../../styles.css'],
-  imports: [ FavoritesComponent, RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, CommonModule],
-  standalone: true
+  imports: [FavoritesComponent, RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule, CommonModule],
+  standalone: true,
 })
 export class HeaderComponent {
+  public readonly matIcons: MatIconRegistry;
+  public favoritesCount: number = 0;
+  @Output() counterChanged: EventEmitter<void> = new EventEmitter<void>();
 
-public readonly matIcons: MatIconRegistry
+  constructor(
+    @Inject(MatIconRegistry) matIcons: MatIconRegistry,
+    private router: Router,
+    private favoritesService: FavoriteCharactersService,
+  ) {
+    this.matIcons = matIcons;
+    matIcons.addSvgIcon(
+      'home',
+      'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+    );
+  }
+  ngOnInit(): void {
+    this.favoritesService.getFavoritesCount().subscribe((count) => {
+      this.favoritesCount = count;
+    });
+  }
 
- constructor(@Inject(MatIconRegistry)matIcons: MatIconRegistry, private router: Router){
-  this.matIcons = matIcons
-  matIcons.addSvgIcon('home', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200')
- }
- navigateToHome(): void {
-  this.router.navigate(['/']);
-}
+  navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
 
-navigateToFavorites(): void {
-  this.router.navigate(['/favorites']);
-}
+  navigateToFavorites(): void {
+    this.router.navigate(['/favorites']);
+  }
 }
--- Mottu-Frontend-RickNMorty/src/app/components/home/home.component.ts
+++ Mottu-Frontend-RickNMorty/src/app/components/home/home.component.ts
@@ -1,93 +1,127 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiResponse, Character } from 'rickmortyapi';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {CommonModule, NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { FavoriteCharactersService } from 'src/app/components/favorites/favorites.service';
 import { CharacterApiService } from 'api/characters.api';
-
+import { NoResultsFoundComponent } from '../no-results-found/no-results-found.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['../../../styles.css'],
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatFormFieldModule, MatInputModule, FormsModule, NgIf, MatButtonModule, MatIconModule, MatCardModule],
-  providers: [CharacterApiService, HttpClientModule]
+  imports: [
+    FooterComponent,
+    CommonModule,
+    RouterOutlet,
+    NoResultsFoundComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+  ],
+  providers: [CharacterApiService, HttpClientModule],
 })
 export class HomeComponent implements OnInit {
   characters: Character[] = [];
-  filteredCharacters: any[] = [];
   searchTerm: string = '';
   currentPage: number = 1;
   totalPages: number = 0;
- 
-  chunkedCharacters: any[] = []; // Define chunkedCharacters array
+  chunkedCharacters: Character[][] = [];
+  filteredCharacters: Character[] = [];
+  favorites: number[] = [];
 
-  constructor(@Inject(CharacterApiService)private characterApiService: CharacterApiService, private favoriteService: FavoriteCharactersService) {
-  }
+  constructor(
+    @Inject(CharacterApiService) private characterApiService: CharacterApiService,
+    private favoriteService: FavoriteCharactersService,
+  ) {}
+
   ngOnInit(): void {
     this.loadCharacters();
   }
 
   loadCharacters(page: number = 1): void {
-    this.characterApiService.getAllCharacters({ page }).then(response => {
-      if (response && response.data && response.data.results) {
-        this.characters = response.data.results.map((character: Character) => ({
-          ...character,
-          isFavorite: this.favoriteService.getFavoriteCharacters().includes(character.id)
-        }));
-        this.filteredCharacters = this.characters;
-        this.totalPages = response.data.info?.pages ?? 0;
-        this.currentPage = page;
-        this.chunkArray(this.filteredCharacters, 3);// Chunk the characters after loading 
-      } else {
-        console.error('Error loading characters: Invalid response format');
-      }
-    }).catch(error => {
-      console.error('Error loading characters:', error);
-    });
+    this.characterApiService
+      .getAllCharacters({ page })
+      .then((response) => {
+        if (response && response.data && response.data.results) {
+          this.characters = response.data.results.map((character: Character) => ({
+            ...character,
+            isFavorite: this.favoriteService.getFavoriteCharacters().includes(character.id),
+          }));
+          this.filteredCharacters = this.characters;
+          this.totalPages = response.data.info?.pages ?? 0;
+          this.currentPage = page;
+          this.chunkArray(this.filteredCharacters, 3);
+        } else {
+          console.error('Error loading characters: Invalid response format');
+        }
+      })
+      .catch((error) => {
+        console.error('Error loading characters:', error);
+      });
+  }
+
+  updateFavoriteCharacters(): void {
+    // Filter the characters array to only include characters that are favorites
+    this.filteredCharacters = this.characters.filter((character) => this.isFavorite(character));
+    // Recalculate chunkedCharacters based on the updated filteredCharacters
+    this.chunkArray(this.filteredCharacters, 3);
   }
 
   addToFavorites(character: Character): void {
     this.favoriteService.addToFavorites(character.id);
-    return 
-  }  
+  }
 
-  removeFromFavorites(character: Character): void { // Change ApiResponse to any since the type of character is any
+  removeFromFavorites(character: Character): void {
     this.favoriteService.removeFromFavorites(character.id);
   }
 
-  searchCharacters(): void {
-    if (this.searchTerm.trim() !== '') {
-      this.filteredCharacters = this.characters.filter(character =>
-        character.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-      );
-    } else {
-      this.filteredCharacters = this.characters;
-    }
-    // Chunk the filtered characters again after filtering
-    this.chunkArray(this.filteredCharacters, 3);
+  isFavorite(character: Character): boolean {
+    return this.favoriteService.getFavoriteCharacters().includes(character.id);
   }
 
-  goToPage(page: number): void {
+  toggleFavorite(character: Character): void {
+    if (this.isFavorite(character)) {
+      this.removeFromFavorites(character);
+    } else {
+      this.addToFavorites(character);
+    }
+  }
+
+  goToPage(page: number) {
     if (page >= 1 && page <= this.totalPages) {
       this.loadCharacters(page);
     }
   }
+  searchCharacters(): void {
+    if (this.searchTerm.trim() !== '') {
+      this.filteredCharacters = this.characters.filter((character) =>
+        character.name.toLowerCase().includes(this.searchTerm.toLowerCase()),
+      );
+    } else {
+      this.filteredCharacters = this.characters;
+    }
 
-  // Function to chunk characters into rows
+    this.chunkArray(this.filteredCharacters, 3);
+  }
+
   chunkArray(array: any[], chunkSize: number): void {
     this.chunkedCharacters = Array(Math.ceil(array.length / chunkSize))
       .fill(null)
       .map((_, index) => index * chunkSize)
-      .map(begin => array.slice(begin, begin + chunkSize));
+      .map((begin) => array.slice(begin, begin + chunkSize));
   }
 }
-
